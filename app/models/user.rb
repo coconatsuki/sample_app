b0e9649cae6a4e59d8e771fcc2835df46ad552aa
Amandine Latour <amandine.latour+github@gmail.com>
2017-10-12 14:24:49 -0400

    Implement basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   validates :name,  presence: true, length: { maximum: 50 }
   validates :password, presence: true, length: { minimum: 6 }
-  
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :email, presence: true, length: { maximum: 255 },
@@ -11,4 +11,12 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
 
   has_secure_password
+
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+  
 end
