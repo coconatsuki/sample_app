93bf555a6da61430f6387e1abd69cd75d76e35a9
Amandine Latour <amandine.latour+github@gmail.com>
2017-10-18 16:41:36 -0400

    still one bug in the test to fix

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,7 @@
 class PasswordResetsController < ApplicationController
   before_action :get_user,   only: [:edit, :update]
   before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]    # In case of expired password reset
 
   def new
   end
@@ -22,19 +23,44 @@ class PasswordResetsController < ApplicationController
   end
 
   def update
+    # In case of A failed update due to an empty password and confirmation :
+    if params[:user][:password].empty?
+      @user.errors.add(:password, "can't be empty")
+      render 'edit'
+    elsif @user.update_attributes(user_params)  # In case of a successful update
+      log_in @user
+      @user.update_attribute(:reset_digest, nil)
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      render 'edit' # In case of a failed update due to an invalid password
+    end
   end
+
 
   private
 
-    def get_user
-      @user = User.find_by(email: params[:email])
-    end
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
 
-    # Confirms a valid user.
-    def valid_user
-      unless (@user && @user.activated? &&
-              @user.authenticated?(:reset, params[:id]))
-        redirect_to root_url
-      end
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  # Confirms a valid user.
+  def valid_user
+    unless (@user && @user.activated? &&
+      @user.authenticated?(:reset, params[:id]))
+      redirect_to root_url
     end
+  end
+
+  # Checks expiration of reset token.
+  def check_expiration
+    if @user.password_reset_expired?
+      flash[:danger] = "Password reset has expired."
+      redirect_to new_password_reset_url
+    end
+  end
 end
