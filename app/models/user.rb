b689fd13f7acbe4fd622134ff70970ce4d661c4a
Amandine Latour <amandine.latour+github@gmail.com>
2017-10-30 13:28:40 -0400

    Showing microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
 
   has_secure_password
-  has_many :microposts
+  has_many :microposts, dependent: :destroy
 
   #--------------------------------METHODS-----------------------------------
 
